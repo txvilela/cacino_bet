e1011bd428cea92d8a2f126650d43768c04bb0f6
Tassio <txvilela96@hotmail.com>
2025-10-20 22:57:27 -0300

    Foi adicionado uma função para somar os pontos do dealer e do player

--- js/iniciar_BJ.js
+++ js/iniciar_BJ.js
@@ -1,12 +1,17 @@
-import { carregarBaralho } from './baralho.js';
+import { baralho, carregarBaralho } from './baralho.js';
 import { darCarta } from './dar_carta.js';
 import { revelarCartaDealer } from './revela_carta_Dealer.js';
 
 const inicio = document.querySelector(".inicio_jogo_container");
 const mesa = document.querySelector(".bJ");
 const botaoInicio = document.querySelector(".inicio_jogo");
+const pontosDealer = document.getElementById("pontosD");
+const pontosPlayer = document.getElementById("pontosP");
 
 let cartaOcultaDealer = null;
+
+pontosDealer.textContent = 0;
+pontosPlayer.textContent = 0;
 
 botaoInicio.addEventListener("click", async () => {
   inicio.style.display = "none";
@@ -15,12 +20,22 @@ botaoInicio.addEventListener("click", async () => {
   await carregarBaralho();
 
 
-  darCarta('player');
-  darCarta('player');
+
+  const carta1 = darCarta('player');
+  const carta2 = darCarta('player');
+  
+  let pontosPlayerTotal = carta1.pontos + carta2.pontos;
+  pontosPlayer.textContent = pontosPlayerTotal;
 
   cartaOcultaDealer = darCarta('dealer', true);
 
-  darCarta('dealer');
+  const cartaVisivelDealer = darCarta('dealer');
+
+  let pontosDealerTotal = cartaVisivelDealer.pontos;
+  pontosDealer.textContent = pontosDealerTotal;
+
+
+
 
   revelarCartaDealer();
 
